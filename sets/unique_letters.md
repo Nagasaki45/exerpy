2252f668a63d968c93d8febf041627302ea95124
Tom Gurion <nagasaki45@gmail.com>
2014-03-13 21:10:33 +0200

    Fixing unique_letters according to Udi instructions

--- sets/unique_letters.md
+++ sets/unique_letters.md
@@ -1,7 +1,7 @@
 Unique letters
 ==============
 
-Get a list of words. Return a string that contains only the words with letter that doesn't present in previous words of the list.
+Get a list of words. Return a string that contains only the words with at least one letter that wasn't present in previous words of the list.
 
 Solve once using lists and once with higher level set operations.
 
@@ -17,6 +17,8 @@ Solve once using lists and once with higher level set operations.
 	l = [unique_letters_without_sets, unique_letters_with_sets]
 
 	for f in l:
-	    assert 'my is' == f(['my', 'name', 'is', 'john', 'doe'])
-	    assert 'hello back' == f(['hello', 'world!', 'hello', 'back'])
-	    assert 'once a' == f(['once', 'upon', 'a', 'time', 'my', 'dog'])
+	    assert 'a ab ac' == f(['a', 'a', 'ab', 'b', 'ac', 'abc'])
+	    assert 'my name is john doe' == f(['my', 'name', 'is', 'john', 'doe'])
+	    assert 'hello world! back' == f(['hello', 'world!', 'hello', 'back'])
+	    assert 'once upon a time dog' == f(['once', 'upon', 'a', 'time', 'dog', 'ate', 'cat'])
+
--- sets/unique_letters.py
+++ sets/unique_letters.py
@@ -2,10 +2,10 @@ def unique_letters_without_sets(words):
     chars = []
     unique_words = []
     for word in words:
-        to_add = True
+        to_add = False
         for c in word:
-            if c in chars:
-                to_add = False
+            if not c in chars:
+                to_add = True
                 break
         if to_add:
             unique_words.append(word)
@@ -18,7 +18,7 @@ def unique_letters_with_sets(words):
     unique_words = []
     for word in words:
         word_set = set([c for c in word])
-        if not word_set & chars:  # set intersection
+        if word_set - chars:  # set difference
             unique_words.append(word)
         chars |= word_set  # set union
     return ' '.join(unique_words)
@@ -27,6 +27,7 @@ def unique_letters_with_sets(words):
 l = [unique_letters_without_sets, unique_letters_with_sets]
 
 for f in l:
-    assert 'my is' == f(['my', 'name', 'is', 'john', 'doe'])
-    assert 'hello back' == f(['hello', 'world!', 'hello', 'back'])
-    assert 'once a' == f(['once', 'upon', 'a', 'time', 'my', 'dog'])
+    assert 'a ab ac' == f(['a', 'a', 'ab', 'b', 'ac', 'abc'])
+    assert 'my name is john doe' == f(['my', 'name', 'is', 'john', 'doe'])
+    assert 'hello world! back' == f(['hello', 'world!', 'hello', 'back'])
+    assert 'once upon a time dog' == f(['once', 'upon', 'a', 'time', 'dog', 'ate', 'cat'])
