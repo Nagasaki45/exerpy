6325e94a9dcf1f87b23440cee0a6ce05b7ca58cc
Tom Gurion <nagasaki45@gmail.com>
2014-06-23 19:57:03 +0300

    Extra pandas magic

--- scientific/pandas_demo.py
+++ scientific/pandas_demo.py
@@ -11,6 +11,8 @@ import matplotlib.pyplot as plt
 NAMES_DIR = 'names'
 URL = 'http://www.ssa.gov/oact/babynames/names.zip'
 
+OUR_NAMES = ['Tom', 'Jacob', 'Alex']
+
 
 def download_and_extract(url, directory):
     print('Downloading archive...')
@@ -54,9 +56,16 @@ def main():
     # analyze name trends
     total_births = names.pivot_table('births', rows='year', cols='name',
                                      aggfunc=sum)
-    total_births[['Tom', 'Jacob', 'Alex']].plot()
+    total_births[OUR_NAMES].plot()
 
     plt.show()
+
+    # http://fivethirtyeight.com/features/
+    # how-to-tell-someones-age-when-all-you-know-is-her-name/
+    names['births_prod_year'] = names['births'] * names['year']
+    grouped = names.groupby('name').sum()
+    mean_birth_year = grouped['births_prod_year'] / grouped['births']
+    print(mean_birth_year.loc[OUR_NAMES])
 
 
 if __name__ == '__main__':
